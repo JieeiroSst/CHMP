2820ecd58c3308c3f3b5475039dd2b1de500fbc2
Alan luu <luumanhquan.91@gmail.com>
2020-05-12 23:06:13 +0700

    add controller customer

--- src/main/java/com/CHMP/controllers/customerController.java
+++ src/main/java/com/CHMP/controllers/customerController.java
@@ -15,9 +15,43 @@ public class customerController {
     @Autowired
     private customerService service;
 
-    @RequestMapping("/comment")
+    @RequestMapping("/customer")
     public String viewPage(Model model){
+        List<customer> customers= service.listAll();
+        model.addAttribute("customer",customers);
+        return "customer";
+    }
 
-        return "";
+    @RequestMapping("/customer/newCustomer")
+    public String showCustomerForm(Model model){
+        customer customer=new customer();
+        model.addAttribute("customer",customer);
+
+        return "customer/newCustomer";
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String save(@ModelAttribute("customer")customer customer){
+        service.save(customer);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/customer/edit/{id}")
+    public ModelAndView showEditCustomerForm(@PathVariable(name = "id")Integer id){
+        ModelAndView modelAndView=new ModelAndView("customer/editCustomer");
+
+        customer customer=service.get(id);
+
+        modelAndView.addObject("customer",customer);
+
+        return modelAndView;
+    }
+
+    @RequestMapping("customer/delete/{id}")
+    public String deleteCustomer(@PathVariable(name = "id")Integer id){
+        service.deleteID(id);
+
+        return "redirect:/";
     }
 }
