2af7660d59bc785f667f7d5f0d49c0119098e03f
Alan luu <luumanhquan.91@gmail.com>
2020-05-12 23:19:19 +0700

    push controller->orderline

--- src/main/java/com/CHMP/controllers/orderLineController.java
+++ src/main/java/com/CHMP/controllers/orderLineController.java
@@ -15,4 +15,41 @@ public class orderLineController {
     @Autowired
     private orderLineService service;
 
+    @RequestMapping("/orderLine")
+    public String showPage(Model model){
+        List<orderLine> orderLines=service.listAll();
+        model.addAttribute("orderLine");
+
+        return "orderLine";
+    }
+
+    @RequestMapping("/orderLine/newOrderLine")
+    public String ShowNewOrderLine(Model model){
+        orderLine orderLine=new orderLine();
+        model.addAttribute("orderLine",orderLine);
+
+        return "orderLine/newOrderLine";
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String save(@ModelAttribute("orderLine")orderLine orderLine){
+        service.save(orderLine);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/orderLine/edit/{id}")
+    public ModelAndView showEditOrderLineForm(@PathVariable(name = "id")Integer id){
+        ModelAndView modelAndView=new ModelAndView("orderLine/editOderLine");
+        orderLine orderLine=service.get(id);
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/orderLine/delete/{id}")
+    public String deleteOrderLine(@PathVariable(name = "id")Integer id){
+        service.deleteID(id);
+
+        return "redirect:/";
+    }
 }
