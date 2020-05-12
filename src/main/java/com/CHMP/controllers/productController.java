e1363b0b083f86c4e6fb92b6b3d0e7bf95eb6204
Alan luu <luumanhquan.91@gmail.com>
2020-05-12 23:28:28 +0700

    push controller product

--- src/main/java/com/CHMP/controllers/productController.java
+++ src/main/java/com/CHMP/controllers/productController.java
@@ -14,4 +14,44 @@ import com.CHMP.services.productService;
 public class productController {
     @Autowired
     private productService service;
+
+    @RequestMapping("product")
+    public String ShowPage(Model model){
+        List<product> products=service.listAll();
+        model.addAttribute("product",products);
+
+        return "product";
+    }
+
+    @RequestMapping("product/newProduct")
+    public String ShowNewProduct(Model model){
+        product product=new product();
+        model.addAttribute("product",product);
+
+        return "product/newProduct";
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product")product product){
+        service.save(product);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping("/product/edit/{id}")
+    public ModelAndView showEditProduct(@PathVariable(name = "id")Integer id){
+        ModelAndView modelAndView=new ModelAndView("product/editProduct");
+
+        product product=new product();
+        modelAndView.addObject("product",product);
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id")Integer id){
+        service.deleteID(id);
+        return "redirect:/";
+    }
+
 }
