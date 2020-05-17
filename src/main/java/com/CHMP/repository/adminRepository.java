a716ac1e74c78f22a389454ed9f18fe45d7565ee
Alan luu <luumanhquan.91@gmail.com>
2020-05-17 23:44:29 +0700

    fix

--- src/main/java/com/CHMP/repository/adminRepository.java
+++ src/main/java/com/CHMP/repository/adminRepository.java
@@ -1,15 +1,10 @@
 package com.CHMP.repository;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.CHMP.DTO.admin;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface adminRepository extends JpaRepository<admin, Integer> {
 
